48ea9668cf3661b8737ee3ce4f0454a5253a9940
Vodu <VoduMapping@gmail.com>
2024-02-13 11:38:16 +0100

    test test

--- Programm/main.py
+++ Programm/main.py
@@ -4,28 +4,45 @@ import sympy
 import numpy as np
 import time
 
-x,y,z = np.meshgrid(np.linspace(-1,1,0.2),np.linspace(-1,1,0.2),np.linspace(-1,1,0.2))
+x,y,z = np.meshgrid(np.arange(-1,1,0.1),np.arange(-1,1,0.1),np.arange(-1,1,0.8))
 
 a,b,c = sympy.symbols("a b c")
 
 sympy.init_printing(use_unicode=True)
 
 #Base function to describe the field
-f_e = 1/(a**2 + b**2 + c**2)
+f_e = sympy.log(a*b*c)
 
 
 #Take the Diffrential to A
-u = sympy.diff(f_e,a)
+u_ = sympy.diff(f_e,a)
 
-k = sympy.diff(f_e,b)
+k_ = sympy.diff(f_e,b)
 
-v = sympy.diff(f_e,c)
-
-
-ax = plt.figure().add_subplot(projection='3d')
+v_ = sympy.diff(f_e,c)
 
 
-qq=plt.quiver(x,y,z,u,v,k,cmap=plt.cm.jet)
+# Create lambdified functions
+u_func = sympy.lambdify((a, b, c), u_, 'numpy')
+k_func = sympy.lambdify((a, b, c), k_, 'numpy')
+v_func = sympy.lambdify((a, b, c), v_, 'numpy')
+
+
+u = u_func(x, y, z)
+k = k_func(x, y, z)
+v = v_func(x, y, z)
+
+ax = plt.figure().add_subplot(111,projection='3d')
+
+print(u)
+
+M = np.sqrt(u**2 + v**2 + k**2)
+
+
+# Get the colormap
+cmap = plt.get_cmap('viridis')
+
+qq=plt.quiver(x,y,z,u,v,k,length=0.1,cmap=cmap,normalize=True,)
 
 plt.colorbar(qq, cmap=plt.cm.jet)
 
