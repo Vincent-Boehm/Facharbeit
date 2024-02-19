7bb161185edbccdbc30d1b275c44b42c4794e8b8
Vodu <VoduMapping@gmail.com>
2024-02-19 17:53:15 +0100

    good work

--- Programm/main.py
+++ Programm/main.py
@@ -4,15 +4,15 @@ import sympy
 import numpy as np
 import time
 
-x,y,z = np.meshgrid(np.arange(-1,1,0.1),np.arange(-1,1,0.1),np.arange(-1,1,0.8))
+x,y, = np.meshgrid(np.arange(-1,1,0.1),np.arange(-1,1,0.1))
 
-a,b,c = sympy.symbols("a b c")
+a,b, = sympy.symbols("a b")
 
 sympy.init_printing(use_unicode=True)
 
 #Base function to describe the field
-f_e = -sympy.sin(a) * -sympy.cos(b) + c
-
+f_e = -sympy.sin(a) * -sympy.cos(b)
+f_e_func = sympy.lambdify((a, b), f_e, 'numpy')
 
 
 #Take the Diffrential to A
@@ -20,33 +20,37 @@ u_ = sympy.diff(f_e,a)
 
 k_ = sympy.diff(f_e,b)
 
-v_ = sympy.diff(f_e,c)
 
 
 # Create lambdified functions
-u_func = sympy.lambdify((a, b, c), u_, 'numpy')
-k_func = sympy.lambdify((a, b, c), k_, 'numpy')
-v_func = sympy.lambdify((a, b, c), v_, 'numpy')
+u_func = sympy.lambdify((a, b), u_, 'numpy')
+k_func = sympy.lambdify((a, b), k_, 'numpy')
 
 
-u = u_func(x, y, z)
-k = k_func(x, y, z)
-v = v_func(x, y, z)
+u = u_func(x, y)
+k = k_func(x, y)
 
-ax = plt.figure().add_subplot(111,projection='3d')
+fig, axs = plt.subplots(2, 1,figsize=(10, 10))
 
-M = np.sqrt(u**2 + v**2 + k**2)
+M = np.sqrt(u**2  + k**2)
 
 
-# Get the colormap
-cmap = plt.get_cmap('viridis')
 
-qq=plt.quiver(x,y,z,u,v,k,length=0.1,cmap=cmap,normalize=True,)
+# Plot the quiver plot in the first subplot
+qq = axs[0].quiver(x, y, u, k, M, cmap=plt.cm.jet)
+axs[0].set_title("elektrische Flussdichte") 
+cbar_qq = plt.colorbar(qq, ax=axs[0])
 
-plt.colorbar(qq, cmap=plt.cm.jet)
+# Plot the contour plot in the second subplot
+pp = axs[1].contourf(x, y, f_e_func(x, y), cmap=plt.cm.jet, levels=100)
+axs[1].set_title('elektrische Ladungsdichte')
+cbar_pp = plt.colorbar(pp, ax=axs[1])
+
+# Adjust layout to prevent overlapping
+plt.tight_layout()
 
 #plt.show()
 
 plt.ioff()
 
-plt.savefig("./images/sin7.png")
+plt.savefig("./Programm/images/1.png")
